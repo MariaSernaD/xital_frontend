7d7ff6f3c02e1d3db4f4a3258e6426a49f4bac3b
MariaSernaD <lups.sernad@gmail.com>
2026-07-19 20:33:10 -0600

    update: include the authProvider from authContext to the App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,10 +5,12 @@ import ProductsPage from "../../pages/ProductsPage";
 import Login from "../../pages/Login";
 import Layout from "../../layout/Layout";
 import Register from "../../pages/Register";
+import { AuthProvider } from "../../context/authContext";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,6 +20,7 @@ export default function App() {
           <Route path="/register" element={<Register/>} />
         </Routes>
       </Layout>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
