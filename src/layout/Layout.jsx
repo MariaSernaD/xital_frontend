ab153b580f3885043e3d73ce620a3593bc6ecf0c
MariaSernaD <lups.sernad@gmail.com>
2026-08-12 16:31:01 -0600

    fix: remove unused isHome and useLocation from Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,6 @@
-import {useLocation} from "react-router-dom";
 import Header from "./Header/Header";
 
 export default function Layout ({children}){
-    const location = useLocation();
-    const isHome = location.pathname === "/";
     return (
         <div className="layout">
             <Header />
